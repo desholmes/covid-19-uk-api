5cb9d2976abff1672b540089777ee9e3ab3f96ef
desholmes <desholmes@gmail.com>
2020-03-18 16:34:48 +0000

    Added Wales totals

--- covid_19_uk/urls.py
+++ covid_19_uk/urls.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 from covid_19_uk.api.views.uk_totals import UkTotalViewSet
 from covid_19_uk.api.views.scotland_totals import ScotlandTotalViewSet
+from covid_19_uk.api.views.wales_totals import WalesTotalViewSet
 
 router = routers.DefaultRouter()
 router.register(r'uk/totals',
@@ -9,4 +10,7 @@ router.register(r'uk/totals',
 router.register(r'scotland/totals',
                 ScotlandTotalViewSet,
                 basename='scotland/totals')
+router.register(r'wales/totals',
+                WalesTotalViewSet,
+                basename='wales/totals')
 urlpatterns = router.urls
